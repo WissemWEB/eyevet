8892123368cc573763800e301a0e081ebd9d0c48
Vasyl Semikhov <vasyl.semikhov@gmail.com>
2019-10-15 19:27:25 +0300

    v2.0.1 images

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,38 @@
-const   gulp = require('gulp'),
-        sass = require('gulp-sass'),
-        autoprefixer = require('gulp-autoprefixer');
-        // rename = require('gulp-rename'),
-        // imagemin = require('gulp-imagemin'),
-        // eslint = require('gulp-eslint'),
-        // merge = require('merge-stream'),
-        // concat = require('gulp-concat'),
-        // uglify = require('gulp-uglify'),
-        // order = require('gulp-order');
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  imagemin = require('gulp-imagemin');
+// rename = require('gulp-rename'),
+// imagemin = require('gulp-imagemin'),
+// eslint = require('gulp-eslint'),
+// merge = require('merge-stream'),
+// concat = require('gulp-concat'),
+// uglify = require('gulp-uglify'),
+// order = require('gulp-order');
 
 var src = 'src/',
-    build = 'build/';
+  build = 'build/';
 
 function style() {
-    return (gulp.src(src + 'scss/*.scss'))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(autoprefixer({
-            // browsers: ['last 4 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest(build + 'css/'));
+  return (gulp.src(src + 'scss/*.scss'))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(autoprefixer({
+      // browsers: ['last 4 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(build + 'css/'));
 }
 
+function images() {
+  return gulp.src(src + 'images/*')
+    .pipe(imagemin({
+      optimizationLevel: 4,
+      progressive: true
+    }))
+    .pipe(gulp.dest(build + 'images/'));
+}
+
+exports.images = images;
 exports.style = style;
